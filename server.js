a3a7e0ea917e9458a09533f7a4ac51a00c810ef4
adam.gajzlerowicz <nelf86@gmail.com>
2017-10-23 22:40:32 +0200

    push

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ import { SubscriptionServer } from 'subscriptions-transport-ws';
 const PORT = 4000;
 const server = express();
 
-server.use('*', cors({ origin: 'http://localhost:3000' }));
+// server.use('*', cors({ origin: 'http://localhost:3000' }));
+server.use('*', cors());
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({
   schema
